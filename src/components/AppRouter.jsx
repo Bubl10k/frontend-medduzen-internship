611cee414215e0b061c1a7ae02597a2ff2cd85b2
Bubl10k <vladislav.zagaiko@gmail.com>
2024-10-30 14:12:04 +0200

    Add lazy loading

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,22 +1,27 @@
 import { Route, Routes } from "react-router-dom";
-import StartPage from "../pages/StartPage";
-import ProfilePage from "../pages/ProfilePage";
-import ListUsersPage from "../pages/ListUsersPage";
-import CompanyListPage from "../pages/CompanyListPage";
-import CompanyProfilePage from "../pages/CompanyProfilePage";
-import AboutPage from "../pages/AboutPage";
+import { lazy, Suspense } from "react";
+import Loading from "./Loading";
+
+const StartPage = lazy(() => import("../pages/StartPage"));
+const ProfilePage = lazy(() => import("../pages/ProfilePage"));
+const ListUsersPage = lazy(() => import("../pages/ListUsersPage"));
+const CompanyListPage = lazy(() => import("../pages/CompanyListPage"));
+const CompanyProfilePage = lazy(() => import("../pages/CompanyProfilePage"));
+const AboutPage = lazy(() => import("../pages/AboutPage"));
 
 const AppRouter = () => {
-    return (
-        <Routes>
-            <Route path="/" element={<StartPage  />}/>
-            <Route path="/about" element={<AboutPage />} />
-            <Route path="/users" element={<ListUsersPage />}/>
-            <Route path="/companies" element={<CompanyListPage />}/>
-            <Route path="/users/:userId" element={<ProfilePage />} />
-            <Route path="/companies/:companyId" element={<CompanyProfilePage />} />
-        </Routes>
-    )
-}
+  return (
+    <Suspense fallback={<Loading />}>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/users" element={<ListUsersPage />} />
+        <Route path="/companies" element={<CompanyListPage />} />
+        <Route path="/users/:userId" element={<ProfilePage />} />
+        <Route path="/companies/:companyId" element={<CompanyProfilePage />} />
+      </Routes>
+    </Suspense>
+  );
+};
 
 export default AppRouter;
