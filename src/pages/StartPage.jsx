eba5634bd2d0e443f3b2c012e0f73d62445d7c5b
Bubl10k <vladislav.zagaiko@gmail.com>
2024-11-01 11:56:57 +0200

    Set up axios

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -1,11 +1,27 @@
-import { Button, Container, Typography } from "@mui/material";
-import { useTranslation } from "react-i18next";
-import DisplayText from "../components/DisplayText";
-import UpdateText from "../components/UpdateText";
+import { Button, Container, Typography } from '@mui/material';
+import { useTranslation } from 'react-i18next';
+import DisplayText from '../components/DisplayText';
+import UpdateText from '../components/UpdateText';
+import { useEffect, useState } from 'react';
+import healthCheck from '../api/healthcheck.service';
 
 const StartPage = () => {
   const { t } = useTranslation();
-  
+  const [status, setStatus] = useState(null);
+
+  const checkBackend = async () => {
+    const response = await healthCheck();
+    if (response.status_code === 200) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  };
+
+  useEffect(() => {
+    checkBackend();
+  }, []);
+
   return (
     <Container maxWidth="sm" sx={{ textAlign: 'center', marginTop: '2rem' }}>
       <Typography variant="h3" gutterBottom>
@@ -19,8 +35,13 @@ const StartPage = () => {
       </Button>
       <DisplayText />
       <UpdateText />
+      {status === true && (
+        <Typography variant="body1" sx={{ marginTop: '2rem' }}>
+          Backend is running
+        </Typography>
+      )}
     </Container>
   );
 };
-  
+
 export default StartPage;
